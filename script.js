76e100d114cd12e7cf0243337eab1565388d535d
adrianna-thomas <42939699+adrianna-thomas@users.noreply.github.com>
2022-04-21 15:21:48 -0400

    Add files via upload
    Added comments and hid alert

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-alert("I am an alert!")
+// alert("I am an alert!")
 
+//Click the share icon and display the new div
 document.querySelector(".share-btn").addEventListener("click", function() {
     document.querySelector(".open-share").style.display = "none";
     document.querySelector(".open-share").style.display = "flex";
 });
 
 
-
+//Click to close the new div
 document.querySelector(".popup-btn").addEventListener("click", function() {
     document.querySelector(".open-share").style.display = "none";
 });
 
+//Closing the div on desktop screen size
 document.querySelector(".close").addEventListener("click", function() {
     document.querySelector(".open-share").style.display = "none";
 });
